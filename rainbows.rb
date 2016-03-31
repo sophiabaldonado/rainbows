b3c3d63442fae56afcc8f78cf5647a66c2632a06
sophia <sophiabaldonado@gmail.com>
2016-03-31 09:44:59 -0700

    Rainbows homework;

--- rainbows.rb
+++ rainbows.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 red = [255,0,0]
 green = [0,255,0]
 blue = [0,0,255]
@@ -27,3 +29,20 @@ end
 
 new_color = mix_colors(red,green)
 puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
+
+rainbow = [
+  { red: { r: 255, g: 0, b: 0 } },
+  { orange: { r: 255, g: 127, b: 0 } },
+  { yellow: { r: 255, g: 255, b: 0 } },
+  { green: { r: 0, g: 255, b: 0 } },
+  { blue: { r: 0, g: 0, b: 255 } },
+  { violet: { r: 75, g: 0, b: 130 } },
+  { purple: { r: 143, g: 0, b: 255 } }
+]
+
+rainbow.each do |color|
+  # puts "#{color.key} colorize(rgb[:r],rgb[:g],rgb[:b])"
+  color_name = color.keys.join
+  color_rgb = color.values.pop
+  puts "#{ color_name } #{ color_rgb[:r] }, #{ color_rgb[:g] }, #{ color_rgb[:b] }".colorize(color_name.to_sym)
+end
